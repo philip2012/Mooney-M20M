e6d1a2ed9cd05d6f963fd8f9ea13a813fc405794
philip2012 <99601790+philip2012@users.noreply.github.com>
2026-04-29 07:44:24 +0700

    Add copyright and license to jsbsim-pacejka.py

--- Tools/jsbsim-pacejka.py
+++ Tools/jsbsim-pacejka.py
@@ -1,6 +1,13 @@
 # Tools/jsbsim-pacejka.py
 # Generate JSBSim CORNERING_COEFF tables using a Pacejka-style Magic Formula.
 
+# Copyright (C) 2026 Philips Nguyen
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+
 import argparse
 import math
 
